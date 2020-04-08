2d2fa6587b91c1755dd360c679eb33137b13e672
Danye <danielbrzen@outlook.com>
2020-04-07 20:11:05 -0500

    CLASE PRINCIPAL DE EJECUCION DE VENTANA GRAFICA DEL PROGRAMA

---
+++ src/main/java/RedFerioviariaJava/InitRedFerrioviaria.java
@@ -0,0 +1,10 @@
+
+package RedFerioviariaJava;
+public class InitRedFerrioviaria {
+
+    public static void main(String[] args) {
+         System.out.println("FUNCIONA");
+         redFerrioviariaGUI TrenesJava = new redFerrioviariaGUI();
+         TrenesJava.setVisible(true);       
+    }    
+}
