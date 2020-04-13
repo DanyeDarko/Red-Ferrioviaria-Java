f1a9568e8a9ee8b1298e668d81f66683c6ff68d9
daniel <danielbrzen@outlook.com>
2020-04-13 12:47:53 -0500

    GUI DE RED FERRIOVIARIA JFRAME JSWING FORM

---
+++ src/main/java/RedFerioviariaJava/ObjetosPrograma/redFerrioviariaGUI.java
@@ -0,0 +1,18 @@
+package RedFerioviariaJava.ObjetosPrograma;
+
+import javax.swing.JFrame;
+
+public class redFerrioviariaGUI extends JFrame {
+
+    public redFerrioviariaGUI() {
+
+        //<editor-fold defaultstate="collapsed" desc="PROPIEDADES DE VENTANA PRINCIPAL">
+        this.setTitle("RED FERRIOVARIA CONCURRENTE"); // TITULO DE VENTANA
+        this.setSize(1000, 700); // TAMAÑO DE LA VENTANA ANCHO::LARGO
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // CIERE DE HILO DE LA VENTANA AL CERRAR
+        //this.add(vias del tren);
+        //</editor-fold>
+        
+    }
+
+}
