2ccf11f7481e8a24eb742495ddd132203e46be79
daniel <danielbrzen@outlook.com>
2020-04-13 13:28:31 -0500

    AGREGAMOS NUEVO LIENZO JPANEL VIAS TREN

--- src/main/java/RedFerioviariaJava/ObjetosPrograma/redFerrioviariaGUI.java
+++ src/main/java/RedFerioviariaJava/ObjetosPrograma/redFerrioviariaGUI.java
@@ -6,11 +6,13 @@ public class redFerrioviariaGUI extends JFrame {
 
     public redFerrioviariaGUI() {
 
+        lienzoViasTren nuevoLienzoVias = new lienzoViasTren();
         //<editor-fold defaultstate="collapsed" desc="PROPIEDADES DE VENTANA PRINCIPAL">
         this.setTitle("RED FERRIOVARIA CONCURRENTE"); // TITULO DE VENTANA
         this.setSize(1000, 700); // TAMAÑO DE LA VENTANA ANCHO::LARGO
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // CIERE DE HILO DE LA VENTANA AL CERRAR
-        //this.add(vias del tren);
+        
+        this.add(nuevoLienzoVias);
         //</editor-fold>
         
     }
